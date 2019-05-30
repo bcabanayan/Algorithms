52a444a2c423f2a49e688983c9aa9d39cd24dd11
bcabanayan <bcabanayan@gmail.com>
2019-05-29 20:20:43 -0700

    Added commenting to stock_prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,12 +3,16 @@
 import argparse
 
 def find_max_profit(prices):
+  # sort prices to find difference between highest and lowest price
   sorted_prices = sorted(prices)
   max_profit_so_far = sorted_prices[0] - sorted_prices[-1]
+  # find buy price 
   for i in range(len(prices)):
     current_min_price_so_far = prices[i]
+    # find max profit by looping through each sell price in the array and subtract buy price from the sell price
     for j in range(i + 1, len(prices)):
       profit = prices[j] - current_min_price_so_far
+      # if profit is greater than current max profit, then update max profit value
       if profit > max_profit_so_far:
         max_profit_so_far = profit
   return max_profit_so_far
