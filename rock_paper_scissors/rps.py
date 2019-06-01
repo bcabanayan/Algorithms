8c07c019674d9c98a469ac92e608cc83959f7e3e
bcabanayan <bcabanayan@gmail.com>
2019-06-01 13:42:49 -0700

    Completed solution for rock paper scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,12 +3,23 @@
 import sys
 
 def rock_paper_scissors(n):
-  plays = [['rock'],['paper'],['scissors']]
-  final_list = [[]]
+  final_list = []
+  options = [['rock'],['paper'],['scissors']]
+  # def add_plays(plays_so_far):
+  #   for i in plays_so_far:
+  #     for j in plays:
+  #       i = i + j
+  #     return plays_so_far
   if n == 0:
-    return final_list
-
-print(rock_paper_scissors(1))
+    return [[]]
+  else:
+    previous_plays = rock_paper_scissors(n - 1)
+    for previous_play in previous_plays:
+      for option in options:
+        final_list.append(previous_play + option)
+  return final_list
+  
+print(rock_paper_scissors(2))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
