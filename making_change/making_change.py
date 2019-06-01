9c9da485c418f84928c10a66d08195f3b535404c
bcabanayan <bcabanayan@gmail.com>
2019-06-01 16:23:54 -0700

    Added function to count number of permutations of denominations to making_change

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -5,8 +5,24 @@ import sys
 # can kind of follow same set up as eating cookies
 # getting from an initial value to zero!
 
-def making_change(amount, denominations):
-  pass 
+# denominations = [1, 5, 10, 25, 50]
+
+def making_change(amount, denominations, ways):
+  if amount == 0:
+    return 1
+  elif amount < 0:
+    return 0
+  elif amount in ways:
+    return ways[amount]
+  else:
+    result = 0
+    for i in range(len(denominations) - 1, -1, -1):
+      result = result + making_change(amount - denominations[i], denominations, ways)
+    ways[amount] = result
+    if result.sort() not in ways:
+      return result
+
+print(making_change(10, [1, 5, 10, 25, 50], {}))
 
 if __name__ == "__main__":
   # Test our your implementation from the command line
